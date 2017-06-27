1f5170a40ea47ce9b950c25a067df1df9286950b
Abby Wilson <abigail.wilson1@outlook.com>
2017-06-27 14:12:09 -0400

    finished code for exercise 5

---
+++ exercise5.rb
@@ -0,0 +1,11 @@
+
+
+def greet_backwards(name)
+  name.reverse + name.reverse
+end
+
+puts "Hello, #{greet_backwards("Amanda")}! Welcome home."
+puts "Hello, #{greet_backwards("Bob")}! Welcome home."
+puts "Hello, #{greet_backwards("Shirley")}! Welcome home."
+puts "Hello, #{greet_backwards("Sue")}! Welcome home."
+puts "Hello, #{greet_backwards("Andy")}! Welcome home."
